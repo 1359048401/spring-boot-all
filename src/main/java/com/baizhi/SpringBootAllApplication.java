2ba2f137d40b3fe6596fa6d7a314dc8e24972565
spj <1359048401@qq.com>
2018-12-17 19:40:08 +0800

    "This is test push into github"

--- src/main/java/com/baizhi/SpringBootAllApplication.java
+++ src/main/java/com/baizhi/SpringBootAllApplication.java
@@ -8,6 +8,7 @@ public class SpringBootAllApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAllApplication.class, args);
+        System.out.println("This is test push into github");
     }
 
 }
